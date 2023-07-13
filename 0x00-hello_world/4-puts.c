3c1e506d6487da1297b673f2484e4fc0f8ef41be
AmrAlnas <amer.live477@gmail.com>
2023-07-13 18:03:17 +0300

    Fifth Task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * main - entry point
- * 
  * Description: A C program that prints using puts
- * 
  * Return: Always returns 0 (Success)
 */
-int main (void){
-    puts("Programming is like building a multilingual puzzle\n");
-    return (0);
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
