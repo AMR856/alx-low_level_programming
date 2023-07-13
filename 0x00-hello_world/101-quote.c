f6a67a37c5c68ffcfcfaa3e7b824cdd98ff1f43d
AmrAlnas <amer.live477@gmail.com>
2023-07-13 20:11:59 +0300

    Advanced 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program that prints a message using fprintf
+ * and this message is printed to the standard error
+ *
+ * Return: Always returns 1 (Failure)
+*/
+
+
+
+int main(void)
+{
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
