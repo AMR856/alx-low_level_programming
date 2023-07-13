cc2b9911d3c9c1e0ba12dc5829782314641071ae
AmrAlnas <amer.live477@gmail.com>
2023-07-13 18:37:47 +0300

    Sixth Task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ *
+ * Description: A C program that prints using printf
+ *
+ * Return: Always returns 0 (Success) 
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
