84744fd13fc813682390acae305e72c1212023c3
AmrAlnas <amer.live477@gmail.com>
2023-07-14 17:05:49 +0300

    Seventh Task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: A C program that prints the number from
+ * 0 to 9 using putchar and some chars mainpulation
+ * Return: Always returns 0 (Success)
+*/
+int main(void)
+{
+	char c;
+
+	c = '0';
+	while (c != '9')
+	{
+		putchar(c);
+		c = c + 1;
+	}
+	putchar('\n');
+	return (0);
+}
