1378489400f416f9ad213b1881735fd54ae44646
AmrAlnas <amer.live477@gmail.com>
2023-07-14 16:08:06 +0300

    Second Task

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Description: A C program that generates a random number and
+ * after that it checks some conditions depending on its value
+ * Return: Always returns 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int last = n % 10;
+
+	printf("Last digit of %d is %d", n, last);
+	if (last == 0)
+	{
+		printf(" and is 0\n");
+	}
+	else if (last > 5)
+	{
+		printf(" and is greater than 5\n");
+	}
+	else if (last < 6 && last != 0)
+	{
+		printf(" and is less than 6 and not 0\n");
+	}
+	return (0);
+}
