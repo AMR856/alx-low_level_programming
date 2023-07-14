6a837fda31f538c2b37bc30eb9f3ada88e0b3b8a
AmrAlnas <amer.live477@gmail.com>
2023-07-14 18:04:34 +0300

    Eighth Task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: A C program that prints the alphabet in reverse order
+ * using putchar function
+ * Return: Always returns 0 (Success)
+*/
+
+int main(void)
+{
+	char c;
+	c = 'z';
+
+	while ( c >= 'a')
+	{
+		putchar(c);
+		c = c - 1;
+	}
+	putchar('\n');
+	return (0);
+}	
+
