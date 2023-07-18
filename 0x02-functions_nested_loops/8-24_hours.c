ebdef10bbb24d7136a129fd13e0758b50fd8a54c
AmrAlnas <amer.live477@gmail.com>
2023-07-18 19:25:22 +0300

    Ninth Task

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - A function that prints all the minutes of the day
+ * Description: This function iterates over the hours of the day
+ * and in each hour it iterates over all minutes in the hour
+ * Return: Always returns 0 (Success)
+*/
+
+void jack_bauer(void)
+{
+	int hours, minutes;
+
+	for (hours = 0; hours <= 24; hours++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0);
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0);
+			_putchar('\n');
+		}
+	}
+	return (0);
+}
