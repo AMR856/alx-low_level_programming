d2547da7bc1b2b61f84cabb81fe3b94b21712958
AmrAlnas <amer.live477@gmail.com>
2023-07-18 18:52:43 +0300

    Eighth Task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
+/**
+ * print_last_digit - A function that prints the last digit of an integer
+ * @r: The number that we want to get its last digit
+ * Description: This function using the reminder method to get
+ * the last digit but first it makes sure that it's dealing with
+ * the absolute value of the number
+ * Return: It returs the last digit
+*/
+
 int print_last_digit(int r)
 {
+	char temp;
+
 	if (r < 0)
 	{
 		r = r * -1;
 	}
-	char temp;
-	while (r > 9)
-	{
-		temp = r % 10;
-		_putchar(temp);
-		r = r / 10;	
-	}
-	if (r > 9)
-	{
-		return temp;
-	}
-	return (0);
+	temp = r % 10;
+	_putchar(temp);
+	return (temp);
 }
