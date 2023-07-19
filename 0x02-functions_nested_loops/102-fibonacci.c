647e3209b1d9325ab2fa9ecf3b0c97498b25b892
AmrAlnas <amer.live477@gmail.com>
2023-07-19 03:47:57 +0300

    Advanced 3

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: A C program that prints the first 50 numbers in fib series
+ * using a for loop and some summation opearations
+ * Return: Always returns 0 (Success)
+*/
+
+int main(void)
+{
+	int counter;
+	unsigned long term1 = 0, term = 1, sum;
+
+	for (counter = 1; counter < 50; counter++)
+	{
+		sum = term1 + term2;
+		term1 = term2;
+		term2 = sum;
+		printf("lu", sum);
+
+		if (counter <= 50)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
