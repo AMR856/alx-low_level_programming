f3ce791ebb044370a295ba630a1ec62a4301ef3a
AmrAlnas <amer.live477@gmail.com>
2023-07-19 03:24:32 +0300

    Advanced 2

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,10 +11,11 @@ void print_multiple(void)
 {
 	int theLimit = 1024;
 	int sum = 0;
+
 	for (int i = 0; i < theLimit; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 			sum = sum + i;
 	}
-	printf("%d",sum);
+	printf("%d", sum);
 }
