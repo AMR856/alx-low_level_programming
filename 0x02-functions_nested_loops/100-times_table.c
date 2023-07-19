f3226e7b974b832dd037af5c7646986a68f2155f
AmrAlnas <amer.live477@gmail.com>
2023-07-19 04:18:54 +0300

    Advanced 1

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_times_table - A function that prints the multiplication table
+ * @n: The number that we use for iteration
+ * Description: This function has two nested loops so that
+ * it can print the required pattern
+*/
+
+void print_times_table(int n)
+{
+	int multi, prod;
+
+	for (int i = 0; i <= n; i++)
+	{
+		_putchar('0');
+		for (multi = 1; multi <= n; multi++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			prod = i * multi;
+			if (prod <= 9)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (prod <= 99)
+			{
+				_putchar(' ');
+				_putchar((prod / 10) + '0');
+			}
+			else if (prod <= 999)
+			{
+				_putchar((prod / 100) + '0');
+				_putchar((prod / 10) - (prod / 100) * 10 + '0');
+			}
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
