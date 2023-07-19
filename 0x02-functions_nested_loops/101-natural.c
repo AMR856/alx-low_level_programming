a92bf6f33b4ef7b92172a5075f14d9b3a75e916c
AmrAlnas <amer.live477@gmail.com>
2023-07-19 03:23:18 +0300

    Advanced 2

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_multiple - A function that prints the multiple of 3
+ * and 5 below 1024 and sums them up
+ * Description: This function uses a for loop and condition using mod operator
+ * to check for the required condition
+*/
+
+void print_multiple(void)
+{
+	int theLimit = 1024;
+	int sum = 0;
+	for (int i = 0; i < theLimit; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+	}
+	printf("%d",sum);
+}
