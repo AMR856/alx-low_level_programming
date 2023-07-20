c3eaf54d8aa1f3b32cb422c56a058d4678c76de8
AmrAlnas <amer.live477@gmail.com>
2023-07-20 16:11:00 +0300

    Eighth Task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - A C progarm that prints some backslashes in stylish way
+ * @n: The number of lines and spaces
+ * Descirption: This program uses two for loops, one from them to print
+ * the spaces and the second one to loop on the number of lines
+*/
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+	}
+}
