ec198dbcba2e330b942fef07065d6f4c4bdcdd28
AmrAlnas <amer.live477@gmail.com>
2023-07-20 16:52:36 +0300

    Tenth Task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: A C program that simulates the game fuzz-buzz by using
+ * a simple for loop from 1 to 100 and some if condition that
+ * depends on mod operator
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
