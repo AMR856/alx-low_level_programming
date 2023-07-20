490131aeb18311d3c94a2c370c4ca4472c3fa083
AmrAlnas <amer.live477@gmail.com>
2023-07-20 16:23:14 +0300

    Eighth Task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,13 +11,21 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		for (j = 0; j < i; j++)
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
-		}
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
 		_putchar('\\');
 		_putchar('\n');
+		}
 	}
 }
