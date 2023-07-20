28312909188309bf6436d1b8900da72ab0a393c7
AmrAlnas <amer.live477@gmail.com>
2023-07-20 21:08:00 +0300

    Eleventh Task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - A C function that prints a right aligned
+ * triangle
+ * @size: The number of rows in the triangle
+ * Description: This function prints the triangle using some for loops
+ * and if conditions
+ * Return: It returns void (Nothing)
+*/
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 1; i <= size; i++)
+	{
+		for (j = size - i - 1; j >= 0; j--)
+		{
+			_putchar(' ');
+		}
+		for (k = 0; k < i; k++)
+		{
+			_putchar('#');
+		}
+		if (i == 1)
+		{
+		}
+		_putchar('\n');
+	}
+}
