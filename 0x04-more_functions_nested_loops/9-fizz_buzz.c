aebbc128fa830d9ce411ec00b89ed68d3a4ab615
AmrAlnas <amer.live477@gmail.com>
2023-07-20 17:00:43 +0300

    Tenth Task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -28,9 +28,13 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
 		else
 		{
-			printf("%d ", i);
+			printf(" %d ", i);
 		}
 	}
 	printf("\n");
