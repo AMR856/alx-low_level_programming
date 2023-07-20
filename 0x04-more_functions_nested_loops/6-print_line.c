6fd5a8a9b17d7f7438458aa6fb38dc6ba27c0cb5
AmrAlnas <amer.live477@gmail.com>
2023-07-20 15:49:07 +0300

    Seventh Task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - A function that prints some lines
+ * Description: This function prints some underscores using
+ * a for loop and a simple condition that checks the number
+ * of underscores that should be printed
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+		return;
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
