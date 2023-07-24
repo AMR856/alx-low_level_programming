8133ba11a458dae8899d6f0a936a04c02c12caa5
AmrAlnas <amer.live477@gmail.com>
2023-07-24 19:27:09 +0300

    Seventh Task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - A function that prints every other char
+ * of a string
+ * @str: The pointer to the string
+ * Description: This function loops through
+ * each other char in the string and prints it on
+ * the stdout using putchar function
+ * and in the end it prints a newline
+ * Return: It returns void (Nothing)
+*/
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str = str + 2;
+	}
+	putchar('\n');
+}
