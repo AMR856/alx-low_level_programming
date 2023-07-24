d54e8b5edc4ba15490ee349a883abdbf1391222d
AmrAlnas <amer.live477@gmail.com>
2023-07-24 18:05:11 +0300

    First Task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - A C function that increments a value that is
- * pointed by a pointer by 1
- * @n: The value to be incremented
+ * reset_to_98 - A C function that change the
+ * value pointed by the pointer to  98
+ * @n: The value to be changed to 98
  * Description: This function all it does is take a pointer then dereference it
- * to add zero to its pointed value
+ * to make it 98
  * Return: It returns void (Nothing)
 */
 
