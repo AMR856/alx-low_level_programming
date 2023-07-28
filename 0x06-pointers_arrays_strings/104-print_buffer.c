fa4cc97698601abeb650973535d4d074b8b57565
AmrAlnas <amer.live477@gmail.com>
2023-07-28 19:15:04 +0300

    Advanced 5

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,53 +1,102 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 /**
- * print_buffer - A function to print the buffer in an array
- * @b: a pointer to the buffer
- * @size: size of the buffer
+ * isPrintable - A function to check if the char can be printed
+ * @myChar: The char to be checked
+ * Return: It returns a boolean true
+ * if it is a printable char and
+ * false if it is not
+*/
+
+bool isPrintable(int myChar)
+{
+	if (myChar >= 32 && myChar <= 127)
+		return (true);
+	else
+		return (false);
+}
+
+
+/**
+ * printAscii - A fucntion to print the Hex
+ * chars in the buffer
+ * @b: A pointer to the buffer
+ * @start: The beginning of printing of Hex values
+ * @end: The end of printing of Hex values
+ * Return: It returns void (Nothing)
+*/
+
+void printHex(char *b, int start, int end)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		if (i < end)
+			printf("%02x", b[i + start]);
+		else
+			printf("  ");
+		if (i % 2)
+			printf(" ");
+
+		i = i + 1;
+	}
+}
+
+/**
+ * printAscii - A fucntion to print the ASCII
+ * chars in the buffer
+ * @b: A pointer to the buffer
+ * @start: The beginning of printing of ASCII values
+ * @end: The end of printing of ASCII values
+ * Return: It returns void (Nothing)
+*/
+
+void printAscii(char *b, int start)
+{
+	int i = 0, myChar;
+
+	while (i < 10)
+	{
+		myChar = b[i + start];
+		if (isPrintable(myChar))
+			printf("%c", myChar);
+		else
+			printf(".");
+		i = i + 1;
+	}
+}
+
+/**
+ * print_buffer - A function to prints a buffer
+ * @b: A pointer to the buffer
+ * @size: The size of the buffer
  * Return: It returns void (Nothing)
 */
 
 void print_buffer(char *b, int size)
 {
-	int j, i;
+	int start, end;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-		printf("\n");
-		return;
-	}
-	for (i = 0; i < size; i += 10)
-	{
-		printf("%08x: ", i);
-		for (j = i; j < i + 10; j += 2)
+		for (start = 0; start < size; start = start + 10)
 		{
-			if (j < size)
-			{
-				printf("%02x", b[j]);
-			}
-			if (j + 1 < size)
-			{
-				printf("%02x ", b[j + 1]);
-			}
+			if (size - start > 10)
+				end = 10;
 			else
-			{
-				printf(" ");
-				printf("   ");
-				printf(" ");
-			}
-		}
-	for (j = i; j < i + 10 && j < size; j++)
-	{
-		if (isprint(b[j]))
-		{
-			printf("%c", b[j]);
-		}
-		else
-		{
-			printf(".");
+				end = size - start;
+			printf("%08x: ", start);
+			printHex(b, start, end);
+			printAscii(b, start);
+			printf("\n");
 		}
 	}
+	else
+	{
 		printf("\n");
 	}
 }
