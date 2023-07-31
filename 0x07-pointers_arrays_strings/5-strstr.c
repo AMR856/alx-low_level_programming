6a7093be73c94fc57b78fe9bf40ca1ae1ac111fb
AmrAlnas <amer.live477@gmail.com>
2023-07-31 22:33:30 +0300

    Fifth Task

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,29 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - A function to get the length of string
- * @s: The pointer to the string
- * Description: This function loops through
- * the string until it finds a null termintor and
- * in each char that is not a null it increments the
- * counter by 1
- * Return: The length of the string
-*/
-
-int _strlen(char *s)
-{
-	int counter;
-
-	counter = 0;
-	while (*s != '\0')
-	{
-		s++;
-		counter++;
-	}
-	return (counter);
-}
-
-/**
  * _strstr - This function checks if a substring
  * can be found in another string
  * @haystack: The string we search in
@@ -35,20 +12,18 @@ int _strlen(char *s)
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
-	int len = _strlen(haystack) - _strlen(needle) + 1;
-
-	for (i = 0; i < len; i++)
+	for (; *haystack; haystack++)
 	{
-		for (j = 0; j < _strlen(needle); j++)
-		{
-			if (haystack[i + j] != needle[j])
-				break;
-			if (j == _strlen(needle) - 1)
-				return (haystack + i);
-		}
+		char *ptr1 = haystack;
+		char *ptr2 = needle;
 
+		while (*ptr1 == *ptr2 && *ptr2 != '\0')
+		{
+			ptr1 = ptr1 + 1;
+			ptr2 = ptr2 + 1;
+		}
+		if (*ptr2 == '\0')
+			return (haystack);
 	}
 	return (NULL);
-
 }
