6c54f83e1b6fadbadab3cad203083c583efc1fcc
AmrAlnas <amer.live477@gmail.com>
2023-07-31 19:10:35 +0300

    First Task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,21 +2,22 @@
 
 /**
  * _memset - a function to set some memory locations to a specfic value
- * @c: Pointer to the array
+ * @s: Pointer to the array
  * @b: The value that we want to set to
  * @n: The number of bytes that we want to use
- * 
+ * Description: Nothing
  * Return: pointer to the array
 */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	char* p = s;
+	char *p = s;
+
 	while (n != 0)
 	{
 		*p = (unsigned char) b;
 		p = p + 1;
 		n = n - 1;
 	}
-	return s;
+	return (s);
 }
