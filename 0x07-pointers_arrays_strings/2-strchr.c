46c1ba91e20b5c4adc6375787f0796adc1446066
AmrAlnas <amer.live477@gmail.com>
2023-07-31 19:46:02 +0300

    Third Task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,12 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		if (*s == c)
-			return (s);
-		s = s + 1;
+		if (s[i] == c)
+			return (s[i]);
 	}
 	return (NULL);
 }
