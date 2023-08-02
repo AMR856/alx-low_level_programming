8d9297f3220eb5776ac5f868da294033f2fe558a
AmrAlnas <amer.live477@gmail.com>
2023-08-02 18:21:35 +0300

    Sixth Task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int _sqrt_wrapper(int, int);
+
 /**
  * _sqrt_recursion - A function that returns the sqrt of a number
  * @n: The number that we want to get its sqrt
@@ -9,15 +11,27 @@
 
 int _sqrt_recursion(int n)
 {
-	int i;
-
 	if (n == 0 || n == 1)
-		return (n);
-	for (i = 1; i <= n / 2; i++)
 	{
-		if (i * i == n)
-			return (i);
+		return (n);
 	}
+	return (_sqrt_wrapper(n, 1));
+}
 
-	return (-1);
+/**
+ * _sqrt_wrapper - A function that helps to find the sqrt
+ * @n: The number that we want to find its square root
+ * @myValue: The starting point of the searching
+ *
+ * Return: It returns the square root of n
+*/
+
+int _sqrt_wrapper(int n, int myValue)
+{
+	if (myValue * myValue == n)
+		return (myValue);
+	else if (myValue * myValue < n)
+		return (_sqrt_wrapper(n, myValue + 1));
+	else
+		return (-1);
 }
