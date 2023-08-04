f92136859660dd038d61683bcf175634d40925da
AmrAlnas <amer.live477@gmail.com>
2023-08-04 16:07:05 +0300

    First Task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,12 +6,13 @@
  * @args: The number of parameters that the program
  * received from the terminal
  * @argv: An array of strings that contains the terimanl's arguments
- * 
+ *
  * Return: Always returns 0 (Success)
 */
 
 int main(int args, char **argv)
 {
-	printf("%s\n",argv[0]);
-	return(EXIT_SUCCESS);
+	printf("%s\n", argv[0]);
+	(void)args;
+	return (EXIT_SUCCESS);
 }
