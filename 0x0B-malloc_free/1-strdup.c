2dd24123f72f4c4e3013f3a2864dc619ba3babfd
AmrAlnas <amer.live477@gmail.com>
2023-08-07 23:25:39 +0300

    Second Task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,28 +3,35 @@
 #include <string.h>
 
 /**
- * _strdup - A function that creates a new array using DMP 
+ * _strdup - A function that creates a new array using DMP
  * and it copies the contents of a given string to the new
  * array that is allocated dynamically
  * @str: Pointer to the given string
  *
- * Return : A pointer to the new allocated array or NULL
+ * Return: A pointer to the new allocated array or NULL
 */
 
 char *_strdup(char *str)
 {
 	unsigned int i;
 
-	char *newArray = (char *)malloc(strlen(str) * sizeof(char));
-	if (str == NULL || newArray == NULL)
+	if (str != NULL)
 	{
-		return (NULL);
+		char *newArray = (char *)malloc(strlen(str) * sizeof(char));
+
+		if (newArray == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for (i = 0; i < strlen(str); i++)
+				strcpy((newArray + i), (str + i));
+			return (newArray);
+		}
 	}
 	else
 	{
-		for (i = 0; i < strlen(str); i++)
-			strcpy((newArray + i), (str + i));
-		return (newArray);
+		return (NULL);
 	}
-
 }
