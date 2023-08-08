411b57b90d697a61e0d39903262f713b7e79c0b0
AmrAlnas <amer.live477@gmail.com>
2023-08-08 15:18:50 +0300

    Advanced 1

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - A function to concatenate command line arguments
+ * @ac: Number of command line arguments
+ * @av: A pointer to the arguments
+ *
+ * Return: NULL or a pointer to dynamically allocated array
+*/
+
+char *argstostr(int ac, char **av)
+{
+	char *result;
+	int i, totalLen = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < ac; i++)
+			totalLen = totalLen + strlen(av[i]);
+		result = (char *)malloc(totalLen * sizeof(char) + 1);
+
+		if (result == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			strcpy(result, av[0]);
+			strcat(result, "\n");
+			for (i = 1; i < ac; i++)
+			{
+				if (i == ac - 1)
+				{
+					strcat(result, av[i]);
+				}
+				else
+				{
+					strcat(result, av[i]);
+					strcat(result, "\n");
+				}
+			}
+		}
+		return (result);
+	}
+}
