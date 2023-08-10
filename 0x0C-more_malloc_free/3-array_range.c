e853c66ad5c7546c1c5863c0b8a3a71b6493e95c
AmrAlnas <amer.live477@gmail.com>
2023-08-11 01:44:24 +0300

    Fourth Task

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * array_range - A function that creates array
+ * and assigns some values to it
+ * @min: The start of the range which
+ * we use to assign values
+ * @max: The end of the range
+ *
+ * Return: Pointer to the created array or NULL
+*/
+
+int *array_range(int min, int max)
+{
+	int strLen, i, counter;
+	int *ptr;
+
+	if (min > max)
+		return (NULL);
+
+	strLen = max - min;
+	ptr = (int *)malloc(strLen * sizeof(int));
+	counter = 0;
+	i = min;
+	for (counter = 0; counter <= strLen; counter++)
+	{
+		ptr[counter] = i;
+		i++;
+	}
+	return (ptr);
+}
