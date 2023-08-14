65e9228b29c681916b1ec50e05f15824d0412f89
AmrAlnas <amer.live477@gmail.com>
2023-08-14 19:11:40 +0300

    First Task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * dog - A struct that makes a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The name of the owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
