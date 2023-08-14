3fdb08290134dba1bf8160be15677e2f268cfb02
AmrAlnas <amer.live477@gmail.com>
2023-08-14 19:30:12 +0300

    Second Task

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - A function to initialize variables
+ * @name: The name that we want to use
+ * @age: The age that we want to use
+ * @owner: The name of the owner that we want to use
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
