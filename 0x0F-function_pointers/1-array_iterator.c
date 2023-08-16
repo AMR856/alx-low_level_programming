6b762fd7d29a0e5bafd6d9b53d007e6ea4a8a8ba
AmrAlnas <amer.live477@gmail.com>
2023-08-16 15:53:08 +0300

    Second Task

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - A function to that loops through the array
+ * @array: Pointer to the array
+ * @size: Size of the array
+ * @action: Pointer to the function to be executed
+ *
+ * Return: It returns void (Nothing)
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
