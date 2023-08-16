18db707d3fc61f69e85ac95f201afa0c4bb90758
AmrAlnas <amer.live477@gmail.com>
2023-08-16 15:55:47 +0300

    Second Task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,8 +11,9 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
-
-	if (size != 0 && action != NULL)
+	
+	
+	if (size != 0 && action != NULL && array != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
