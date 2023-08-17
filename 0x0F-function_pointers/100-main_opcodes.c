62f888da7a6f8a3334d540555ac6065181206cb4
AmrAlnas <amer.live477@gmail.com>
2023-08-17 04:20:15 +0300

    Advanced 1

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -12,7 +12,7 @@
 void printOpCodes(const void *mainAddr, size_t mySize)
 {
 	size_t i;
-	char *bytes = (char *)mainAddr;
+	const unsigned char *bytes = (const unsiged char *)mainAddr;
 
 	for (i = 0; i < mySize; i++)
 	{
