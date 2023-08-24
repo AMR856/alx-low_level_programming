0432de0508b881828fefb1a801e6cca7075c6cd2
AMR856 <amer.live477@gmail.com>
2023-08-24 18:48:01 +0300

    Fifth Task

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - A function to free the linked list
+ * @head: A pointer to the head of the linked list
+ *
+ * Return: It returns nothing (void)
+*/
+
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	head = NULL;
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -27,5 +27,6 @@ size_t print_list(const list_t *h);
 int _strlen(const char *myString);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
