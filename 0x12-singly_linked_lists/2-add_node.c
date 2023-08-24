9f58f1468ef5aa791b76311752c443142415a068
AMR856 <amer.live477@gmail.com>
2023-08-24 15:18:44 +0300

    Third Task

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node - A function to add element at the beginning
+ * of the linked list
+ * @head: A pointer to the head of the linked list
+ * @str: The string the to be copied into
+ * the new created node
+ *
+ * Return: It returns the address of the newly created
+ * element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newElement;
+	
+	newElement = (list_t *)malloc(sizeof(list_t));
+	if (newElement == NULL)
+		return (NULL);
+	newElement->str = strdup(str);
+	newElement->len = _strlen(str);
+	newElement->next = *head;
+	*head = newElement;
+	return (newElement);	
+}
+
+/**
+ * _strlen - A function to get the length of a string
+ * @myString: A pointer to the required string
+ *
+ * Return: It returns the length of the string
+*/
+
+int _strlen(const char *myString)
+{
+	int i = 0;
+	const char *theIterator = myString;
+	while (*theIterator)
+	{
+		i = i + 1;
+		theIterator = theIterator + 1;
+	}
+	return (i);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,5 +24,7 @@ typedef struct list_s
 
 size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
+int _strlen(const char *myString);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
