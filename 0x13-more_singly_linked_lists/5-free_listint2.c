f01a081dd89f0bf10638b80ecfbe43cefc1edabb
AMR856 <amer.live477@gmail.com>
2023-08-28 18:53:42 +0300

    Sixth Task

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - A function to free a linked list
+ * @head: A pointer that points to a pointer which points
+ * to the head of the linked list
+ *
+ * Return: It returns nothing (void)
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
