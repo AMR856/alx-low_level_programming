5aacbcc96d05e6ba77fcbb45b5b71634e9792409
AMR856 <amer.live477@gmail.com>
2023-08-30 01:57:50 +0300

    Sixth Task

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,17 +10,17 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
+	listint_t *current;
 	listint_t *next;
 
 	if (head == NULL)
 		return;
-
+	current = *head;
 	while (current != NULL)
 	{
-		next = current;
-		current = current->next;
-		free(next);
+		next = current->next;
+		free(current);
+		current = next;
 	}
 	*head = '\0';
 	head = '\0';
