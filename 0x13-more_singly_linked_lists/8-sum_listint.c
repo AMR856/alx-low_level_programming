8320991c557a30a6bd1686f494b6d9bd0804fcce
AMR856 <amer.live477@gmail.com>
2023-08-29 04:26:27 +0300

    Ninth Task

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_listint - A function to get the sum of data in a linked list
+ * @head: A pointer to the head of the linked list
+ * 
+ * Return: It returns the sum of the data in the linked list
+*/
+
+int sum_listint(listint_t *head)
+{
+	int theSum = 0;
+	listint_t *currentNode;
+
+	if (head == NULL)
+		return (0);
+	currentNode = head;
+	while (currentNode != NULL)
+	{
+		theSum = theSum + (*currentNode).n;
+		currentNode = (*currentNode).next;
+	}
+	return (theSum);
+}
