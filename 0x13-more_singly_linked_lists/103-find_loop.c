deba1867c7d38e6f61fc883a4c10061d1a8f1f3e
AMR856 <amer.live477@gmail.com>
2023-08-31 02:46:08 +0300

    Advanced 4

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
+/**
+ * find_listint_loop - A function to find a loop in the linked list
+ * @head: A pointer to the head of the linked list
+ *
+ * Return: It returns the address of the node
+ * in which the loop starts
+*/
 
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slowPoi;
 	listint_t *fastPoi;
-	
+
 	if (head == NULL)
 		return (NULL);
 	slowPoi = head, fastPoi = head;
