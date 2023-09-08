642908fbd7d35cc5667eb31bc21e72153e782efb
AMR856 <amer.live477@gmail.com>
2023-09-08 16:17:25 +0300

    Third Task

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+/**
+ * append_text_to_file - A function to append text to the end
+ * of the file
+ * @filename: A pointer to the filename
+ * @text_content: A pointer to the content to be printed
+ *
+ * Return: 1 or -1
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int myFileD,conditionPara;
+
+	if (filename == NULL)
+		return (-1);
+	myFileD = open(filename, O_APPEND | O_WRONLY);
+	if (myFileD < 0)
+		return (-1);
+	if (text_content != NULL)
+	{
+		conditionPara = write(myFileD, text_content, strlen(text_content));
+		if (conditionPara < 0)
+		{
+			close(myFileD);
+			return (-1);
+		}
+	}
+	else
+	{	close(myFileD);
+		return (1);
+	}
+	close(myFileD);
+	return (1);
+}
