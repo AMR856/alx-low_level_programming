a0c1ead459f241d1dda70c54d87ad8343bd8074d
AMR856 <amer.live477@gmail.com>
2023-10-12 17:05:50 +0300

    dk

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,7 +12,9 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newNode;
-
+	
+	if (*head == NULL)
+		return (NULL);
 	newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
 		return (NULL);
