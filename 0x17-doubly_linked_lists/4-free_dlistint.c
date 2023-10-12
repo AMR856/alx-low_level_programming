62bababfe9267325baf7fe8b67f0a85307b68468
AMR856 <amer.live477@gmail.com>
2023-10-12 18:16:19 +0300

    Try

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - A function to free a doubly linked list
+ * @head: A pointer to the head
+ * 
+ * Return: Nothing (void)
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *tempHead, *tempHead1;
+    
+    tempHead = head;
+    while (tempHead != NULL)
+    {
+        tempHead1 = tempHead->next;
+        free(tempHead);
+        tempHead = tempHead1;
+    }
+    head = NULL;
+}
