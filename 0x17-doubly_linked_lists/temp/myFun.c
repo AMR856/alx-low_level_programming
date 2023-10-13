50703023111f52acd8b27e3d53be215fe214635f
AMR856 <amer.live477@gmail.com>
2023-10-13 12:46:42 +0300

    ok

--- 0x17-doubly_linked_lists/temp/myFun.c
+++ 0x17-doubly_linked_lists/temp/myFun.c
@@ -5,42 +5,36 @@
  * at a specfic index
  * @head: A pointer to a pointer that points to the head
  * @index: Index of the deleted node
- * 
+ *
  * Return: 0 or 1
 */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *myTempHead = *head;
-	unsigned counter = 0;
+	unsigned int counter = 0;
 
 	if (*head == NULL)
 		return (-1);
 	if (index == 0)
 	{
-		myTempHead->next->prev = NULL;
-		myTempHead->prev = NULL;
 		*head = myTempHead->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
 		free(myTempHead);
 		return (1);
 	}
-	while (myTempHead != NULL)
+	while (myTempHead != NULL && counter < index)
 	{
-		if (counter == index)
-		{
-			myTempHead->prev->next = myTempHead->next;
-			myTempHead->next = myTempHead->prev;
-			free(myTempHead);
-			return (1);
-		}
-		counter++;
 		myTempHead = myTempHead->next;
+		counter++;
 	}
-	if (index == counter)
-	{
-		myTempHead->prev->next = NULL;
-		free(myTempHead);
-		return (1);
-	}
-	return (-1);
+	if (myTempHead == NULL)
+		return (-1);
+	if (myTempHead->prev != NULL)
+		myTempHead->prev->next = myTempHead->next;
+	if (myTempHead->next != NULL)
+		myTempHead->next->prev = myTempHead->prev;
+	free(myTempHead);
+	return (1);
 }
