dfc9b7b5514ba8c46f733e21ae8ad3e30d0bbd10
AMR856 <amer.live477@gmail.com>
2023-11-02 20:20:15 +0200

    dk actually but go

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - it's a function to set a node in the array
+ * @ht: A pointer to my table
+ * @key: The key of my node
+ * @value: The value to be put in the node
+ *
+ * Return: 1 on success and 0 on failure
+*/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int myIndex;
+	hash_node_t *myNewNode = (hash_node_t *)malloc(sizeof(hash_node_t));
+
+	if (ht == NULL)
+		return (0);
+	if (myNewNode == NULL)
+		return (0);
+	myNewNode->key = (char *)malloc(strlen(key) + 1);
+	myNewNode->value = (char *)malloc(strlen(value) + 1);
+	myNewNode->next = NULL;
+	strcpy(myNewNode->value, value);
+	strcpy(myNewNode->key, key);
+	myIndex = key_index((const unsigned char *)myNewNode->key, ht->size);
+	if (ht->array[myIndex] != NULL)
+		myIndex = 0;
+	ht->array[myIndex] = myNewNode;
+	return (1);
+}
