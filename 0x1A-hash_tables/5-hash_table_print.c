e5177f56ddb5adeecdec2b817ad34c55488bcd27
AMR856 <amer.live477@gmail.com>
2023-11-03 10:55:21 +0200

    Trying on the morning

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - A function to print my hash table
+ * @ht: A pointer to the head of the table
+ *
+ * Return: nothing (Void)
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	int myFlag = 0;
+	hash_node_t *current;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (myFlag)
+			{
+				printf(", ");
+				myFlag = 0;
+			}
+			current = ht->array[i];
+			printf("\'%s\': ", current->key);
+			while (current != NULL)
+			{
+				printf("\'%s\'", current->value);
+				if (current->next != NULL)
+					printf(" ");
+				current = current->next;
+			}
+			myFlag = 1;
+		}
+	}
+	printf("}\n");
+}
